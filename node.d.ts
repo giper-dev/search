5c9974e78dac6515da38292983994d39f52fdf37
nin-jin <nin-jin@users.noreply.github.com>
2026-01-24 07:12:08 +0000

    Deploying to gh-pages from @ giper-dev/search@3e5492430071614a4fe01f42f72adb671f636a59 🚀

--- node.d.ts
+++ node.d.ts
@@ -2897,6 +2897,7 @@ declare namespace $ {
 		bubble_content( ): readonly(any)[]
 		Filter( ): $mol_search
 		Trigger_icon( ): $mol_icon_dots_vertical
+		trigger_enabled( ): ReturnType< $mol_select['enabled'] >
 	}
 	
 }
