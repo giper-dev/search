96b1202f7f683de1a9f3893986c22e27ca4b9e76
nin-jin <nin-jin@users.noreply.github.com>
2026-03-17 07:45:42 +0000

    Deploying to gh-pages from @ giper-dev/search@16c9875050a1fbc90954a76d600023a670bbe14e 🚀

--- node.mjs
+++ node.mjs
@@ -5472,6 +5472,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- node.test.js
+++ node.test.js
@@ -5463,6 +5463,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
