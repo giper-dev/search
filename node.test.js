c626e9ea29fc434680c177064a99c4dcd90b0d06
nin-jin <nin-jin@users.noreply.github.com>
2026-03-24 07:45:45 +0000

    Deploying to gh-pages from @ giper-dev/search@16c9875050a1fbc90954a76d600023a670bbe14e 🚀

--- node.test.js
+++ node.test.js
@@ -7577,6 +7577,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
